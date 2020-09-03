4073bb7271665dd64cf2b4b05a5a0346c53dd065
enthusiast17 <ulan17nurym@gmail.com>
2020-09-03 23:20:28 +0500

    feat: add some features

--- core.js
+++ core.js
@@ -1,12 +1,14 @@
 /*
     State
 */
+
 import state from './state.js'
 
 
 /*
     Element
 */
+
 export const createElement = (type) => document.createElement(type)
 
 export const changeElementAttributes = (element, attributes) => Object.assign(element, attributes)
@@ -15,25 +17,53 @@ export const changeElementAttributes = (element, attributes) => Object.assign(el
 /*
     Container
 */
-export const createContainer = (attributes) => {
-    const element = createElement('div')
-    const parent = attributes.parent || document.body
-    attributes = deleteAttributes(attributes, ['type', 'parent', 'name'])
-    changeElementAttributes(element, attributes)
-    state.containers.push({
-        'id': attributes.id,
-        'container': {
-            'isHidden': () => element.style.display === 'none' || element.style.display === '' || element.style.display === undefined,
-            'hide': () => element.style.display = 'none',
-            'show': (type) => element.style.display = type
-        }, 
-        'element': element, 
-        'parent': parent
-    })
+
+export const createContainer = (obj) => {
+    if (obj.children.length !== 0 && 'tagName' in obj.parent && obj.parent.tagName !== 'DIV' && obj.parent.tagName !== 'BODY' ||
+        obj.children.length !== 0 && 'type' in obj.parent && obj.parent.type !== 'div') {
+        throw new Error("The key 'children' must be empty if the key 'type' value is not 'div'")
+    } 
+
+    return obj.children.reduce((parent, child) => {
+        if (child.children.length === 0) {
+            parent.appendChild(toElement(child.parent))
+        } else {
+            parent.appendChild(toElement(createContainer(child)))
+            addContainerToState(parent)
+        }
+        return parent
+    }, toElement(obj.parent))
+}
+
+const addContainerToState = (element) => {
+    if (element.tagName === 'BODY') return
+    if (element.tagName === 'DIV' && !isContainerExist(element.id)) {
+        state.containers.push({
+            'id': obj.id,
+            'container': {
+                'isHidden': () => element.style.display === 'none' || element.style.display === '' || element.style.display === undefined,
+                'hide': () => element.style.display = 'none',
+                'show': (type) => element.style.display = type
+            }, 
+            'element': element, 
+        })
+    }
+}
+
+const toElement = (obj) => {
+    if (obj.tagName === 'BODY' || obj.tagName === 'DIV') return obj
+    const element = createElement(obj.type)
+    obj = deleteAttributes(obj, ['type', 'parent'])
+    changeElementAttributes(element, obj)
+    return element
 }
 
 const deleteAttributes = (attributes, keys) => Object.fromEntries(Object.entries(attributes).filter(([k, v]) => !keys.includes(k)))
 
 export const getContainer = (id) => state.containers.find((element) => element.id === id).container
+
+export const getContainerElement = (id) => state.containers.find((element) => element.id === id).element
+
+export const isContainerExist = (id) => state.containers.every((element) => element.id === id)
 
 export const renderContainers = () => state.containers.forEach((element) => element.parent.appendChild(element.element))
