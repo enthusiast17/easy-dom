34cd824c62bf0a7627a2589abaf495bb384fc4be
enthusiast17 <ulan17nurym@gmail.com>
2020-09-10 16:29:34 +0500

    feat: add eventListener

--- core.js
+++ core.js
@@ -11,7 +11,8 @@ import state from './state.js'
 
 export const createElement = (obj) => {
     const element = document.createElement(obj.type)
-    obj = deleteAttributes(obj, ['type'])
+    if (obj.eventListener !== undefined) element.addEventListener(obj.eventListener.type, obj.eventListener.callback)
+    obj = deleteAttributes(obj, ['type', 'eventListener'])
     changeElementAttributes(element, obj)
     return element
 }
@@ -24,52 +25,33 @@ export const changeElementAttributes = (element, attributes) => Object.assign(el
 */
 
 export const createContainer = (obj) => {
-    const parseObject = (obj) => {
-        if (obj.children.length !== 0 && 'tagName' in obj.parent && obj.parent.tagName !== 'DIV' && obj.parent.tagName !== 'BODY' ||
+    if (obj.children.length !== 0 && 'tagName' in obj.parent && obj.parent.tagName !== 'DIV' && obj.parent.tagName !== 'BODY' ||
         obj.children.length !== 0 && 'type' in obj.parent && obj.parent.type !== 'div') {
-            throw new Error("The key 'children' must be empty if the key 'type' value is not 'div'")
-        } 
+        throw new Error("The key 'children' must be empty if the key 'type' value is not 'div'")
+    } 
 
-        return obj.children.reduce((parent, child) => {
-            if (child.children.length === 0) {
-                parent.appendChild(toElement(child.parent))
-            } else {
-                parent.appendChild(toElement(parseObject(child)))
-                addContainerToState(parent)
-            }
-            return parent
-        }, toElement(obj.parent))
-    }
-    
-    parseObject(obj)
+    return obj.children.reduce((parent, child) => {
+        if (child.children.length === 0) {
+            parent.appendChild(toElement(child.parent))
+        } else {
+            parent.appendChild(toElement(createContainer(child)))
+            addContainerToState(parent)
+        }
+        return parent
+    }, toElement(obj.parent))
 }
 
 const addContainerToState = (element) => {
     if (element.tagName === 'BODY') return
     if (element.tagName === 'DIV' && !isContainerExist(element.id)) {
-        state.containers.push({
-            'id': obj.id,
-            'container': {
-                'isHidden': () => element.style.display === 'none' || element.style.display === '' || element.style.display === undefined,
-                'hide': () => element.style.display = 'none',
-                'show': (type) => element.style.display = type
-            }, 
-            'element': element, 
-        })
+        state.containers.push(element)
     }
 }
 
-const toElement = (obj) => {
-    if (obj.tagName === 'BODY' || obj.tagName === 'DIV') return obj
-    return createElement(obj)
-}
+const toElement = (obj) => (obj.tagName === 'BODY' || obj.tagName === 'DIV') ? obj : createElement(obj)
 
 const deleteAttributes = (attributes, keys) => Object.fromEntries(Object.entries(attributes).filter(([k, v]) => !keys.includes(k)))
 
-export const getContainer = (id) => state.containers.find((element) => element.id === id).container
-
-export const getContainerElement = (id) => state.containers.find((element) => element.id === id).element
+export const getContainer = (id) => state.containers.find((element) => element.id === id).container.element
 
 export const isContainerExist = (id) => state.containers.every((element) => element.id === id)
-
-export const renderContainers = () => state.containers.forEach((element) => element.parent.appendChild(element.element))
